72fb4ed23f70d6b4f906c67ecc5295faadc57bc8
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2024-02-01 20:57:56 +0100

    created product.service.ts in order to retrieve list of products based on search mode or category mode and created product.component.ts which consume services data added route in app.module.ts and some imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './components/products/products.component';
+
 import { ProductCategoryOffCanvaComponent } from './components/product-category-off-canva/product-category-off-canva.component';
 import {HttpClientModule} from "@angular/common/http";
+import { ProductComponent } from './components/product/product.component';
+import {RouterModule, Routes} from "@angular/router";
+
+
+
+const routes: Routes = [
+  {path: 'products', component: ProductComponent},
+  {path: 'category', component: ProductComponent},
+  {path: 'products/:id', component: ProductComponent},
+  {path: 'category/:keyword', component: ProductComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent,
     ProductCategoryOffCanvaComponent,
+    ProductComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
     [HttpClientModule]
   ],
   providers: [],
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -37,6 +37,18 @@ export class ProductService{
     return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
+  //method to get products by keyword from backend for search component
+  searchProductsPaginate(thePage:number,
+                         thePageSize:number,
+                         theKeyword: string): Observable<GetResponseProducts> {
+
+    //need to build URL based on keyword  and page size
+    const searchUrl = `${this.productUrl}/search/findByNameContaining?name=${theKeyword}`
+      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
 
 
 }
