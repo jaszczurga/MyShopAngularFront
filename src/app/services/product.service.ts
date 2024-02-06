08633f8c16e7028c708daf0f89787c51dc0c3a10
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2024-02-06 12:31:07 +0100

    changed Url in product.service.ts to variable from enviroment

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,6 +14,7 @@ export class ProductService{
 
   categoryUrl = environment.springBootApiUrlhttp + "/productCategories";
   productUrl = environment.springBootApiUrlhttp + "/products";
+  actionApiUrl = environment.springBootApiUrlhttp + "/action";
 
 
   constructor( private httpClient: HttpClient ) {
@@ -34,7 +35,7 @@ export class ProductService{
                           theCategoryId: number ): Observable<GetResponseProductsModel> {
 
     //new version of api
-    const searchUrl = `http://localhost:8080/api/action/productsByCategoryId?categoryId=${theCategoryId}&page=${thePage}&size=${thePageSize}`
+    const searchUrl = this.actionApiUrl + `/productsByCategoryId?categoryId=${theCategoryId}&page=${thePage}&size=${thePageSize}`
 
     //new version of api
     return this.httpClient.get<GetResponseProductsModel>(searchUrl);
@@ -45,7 +46,7 @@ export class ProductService{
                           thePageSize: number,
                           theKeyword: string ): Observable<GetResponseProductsModel> {
     ///////////////////////////search component new version of api////////////////////////
-    const searchUrl = `http://localhost:8080/api/action/productsContainingName?name=${theKeyword}&page=${thePage}&size=${thePageSize}`
+    const searchUrl = this.actionApiUrl + `/productsContainingName?name=${theKeyword}&page=${thePage}&size=${thePageSize}`
     //new version of api
     return this.httpClient.get<GetResponseProductsModel>(searchUrl);
 
@@ -61,49 +62,49 @@ export class ProductService{
   //update products category
   updateProductCategory(theProduct: ProductDto,id: number): Observable<Product> {
     console.log('theProduct=' + JSON.stringify(theProduct));
-    return this.httpClient.patch<Product>(`http://localhost:8080/api/action/updateProduct/${id}`, theProduct);
+    return this.httpClient.patch<Product>(  this.actionApiUrl +`/updateProduct/${id}`, theProduct);
   }
 
   //method to delete product by id
   deleteProductById(id: number): Observable<any> {
-    return this.httpClient.delete(`http://localhost:8080/api/action/deleteProduct/${id}`);
+    return this.httpClient.delete(  this.actionApiUrl +`/deleteProduct/${id}`);
   }
 
   //delete category by id
   deleteCategoryById(id: number): Observable<any> {
-    return this.httpClient.delete(`http://localhost:8080/api/action/deleteCategory/${id}`);
+    return this.httpClient.delete(  this.actionApiUrl +`/deleteCategory/${id}`);
   }
 
   //get number of products in category
   getNumberOfProductsInCategory(theCategoryId: number): Observable<number> {
-    const searchUrl = `http://localhost:8080/api/action/numberOfProducts/${theCategoryId}`;
+    const searchUrl =   this.actionApiUrl +`/numberOfProducts/${theCategoryId}`;
     return this.httpClient.get<number>(searchUrl);
   }
 
   //get product by id
   getProductById(id: string): Observable<Product> {
-    const searchUrl = `http://localhost:8080/api/action/product/${id}`;
+    const searchUrl =  this.actionApiUrl + `/product/${id}`;
     return this.httpClient.get<Product>(searchUrl);
   }
 
   //save new category to db
   saveCategory(theCategory: CategoryDto): Observable<CategoryDto> {
-    return this.httpClient.post<CategoryDto>("http://localhost:8080/api/action/saveCategory", theCategory);
+    return this.httpClient.post<CategoryDto>(  this.actionApiUrl +"/saveCategory", theCategory);
   }
 
   //update existing category name in db
   updateCategory(theCategory: CategoryDto,id:string): Observable<CategoryDto> {
-    return this.httpClient.patch<CategoryDto>(`http://localhost:8080/api/action/updateCategory/${id}`, theCategory);
+    return this.httpClient.patch<CategoryDto>(  this.actionApiUrl +`/updateCategory/${id}`, theCategory);
   }
 
   //method to save product to db
   saveProduct(theProduct: ProductDto): Observable<ProductDto> {
-    return this.httpClient.post<ProductDto>("http://localhost:8080/api/action/saveProduct", theProduct);
+    return this.httpClient.post<ProductDto>(  this.actionApiUrl +"/saveProduct", theProduct);
   }
 
   //method to update product
   updateProduct(theProduct: ProductDto,id: string): Observable<ProductDto> {
-    return this.httpClient.patch<ProductDto>(`http://localhost:8080/api/action/updateProduct/${id}`, theProduct);
+    return this.httpClient.patch<ProductDto>(  this.actionApiUrl +`/updateProduct/${id}`, theProduct);
   }
 
 
