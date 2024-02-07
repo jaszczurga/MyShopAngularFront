cf66afef36ea6f456b4d71bf2fb6dbcf7d862bb7
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2024-02-07 22:21:39 +0100

    added image to details view of product in  user screen

--- src/app/components/product-content-details/product-content-details.component.ts
+++ src/app/components/product-content-details/product-content-details.component.ts
@@ -3,6 +3,7 @@ import {Product} from "../../common/product";
 import {ProductCategory} from "../../common/product-category";
 import {ActivatedRoute} from "@angular/router";
 import {ProductService} from "../../services/product.service";
+import {ImageService} from "../../services/image.service";
 
 @Component({
   selector: 'app-product-content-details',
@@ -14,7 +15,8 @@ export class ProductContentDetailsComponent implements OnInit{
   public product: Product = new Product(1, "Product 1", "This is a product", 100, 100, [], new ProductCategory(1, "Category 1",new Array())) ;
 
     constructor(private productService: ProductService,
-                private route:ActivatedRoute) {
+                private route:ActivatedRoute,
+                private imageService: ImageService) {
     }
 
     ngOnInit(): void {
@@ -25,8 +27,22 @@ export class ProductContentDetailsComponent implements OnInit{
       this.productService.getProductById(id).subscribe(
         data => {
           this.product = data;
+          this.listImagesForProduct(this.product.id);
         }
       )
     }
+
+
+  images: any[] = [];
+  listImagesForProduct(id: number) {
+    this.imageService.getImagesOfProduct(id).subscribe(
+      data => {
+        this.images = data;
+        console.log("images=iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+        console.log(this.images);
+      }
+    );
+  }
+
 
 }
