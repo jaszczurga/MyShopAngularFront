a9a2ea572e9f394fad10ec4d23b299c126a0dff7
jaszczurga <71510646+jaszczurga@users.noreply.github.com>
2024-03-07 21:26:38 +0100

    sedning messages as a manager to customer works

--- src/app/components/live-chat/live-chat.component.ts
+++ src/app/components/live-chat/live-chat.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Message} from "../../common/message";
 import {LiveChatServiceService} from "../../services/live-chat-service.service";
 
+
 @Component({
   selector: 'app-live-chat',
   templateUrl: './live-chat.component.html',
@@ -10,28 +11,44 @@ import {LiveChatServiceService} from "../../services/live-chat-service.service";
 export class LiveChatComponent implements OnInit {
   messagesFromManager: {content:String}[] = [];
   messagesFromCustomer: {content:String}[] = [];
-  message : string = '';
+  message : string = "";
+  liveChatService: LiveChatServiceService;
+  constructor(liveChatService: LiveChatServiceService) {
+    this.liveChatService = liveChatService;
+  }
 
-  constructor(private liveChatService: LiveChatServiceService) {
+  sendMessage(){
+    if(this.message.length > 0){
+      this.liveChatService.sendMessage({
+        receiverId: 3,
+        content: this.message,
+        senderId: 3
+      });
+      this.message = "";
+    }
   }
 
   ngOnInit(): void {
-    this.liveChatService.getMessagesFromManager().subscribe(data => {
-      this.messagesFromManager.push(data);
-    });
 
-    this.liveChatService.getMessagesFromCustomer().subscribe(data => {
-      this.messagesFromCustomer.push(data);
-    });
+
+
+
+    // this.liveChatService.getMessagesFromManager().subscribe(data => {
+    //   this.messagesFromManager.push(data);
+    // });
+    //
+    // this.liveChatService.getMessagesFromCustomer().subscribe(data => {
+    //   this.messagesFromCustomer.push(data);
+    // });
   }
 
-  sendMessageToManager() {
-    const message = new Message(this.message, 'managerId', 'customerId');
-    this.liveChatService.sendMessageToManager(message);
-  }
-
-  sendMessageToCustomer() {
-    const message = new Message(this.message, "3", "3");
-    this.liveChatService.sendMessageToCustomer(message);
-  }
+  // sendMessageToManager() {
+  //   const message = new Message(this.message, 'managerId', 'customerId');
+  //   this.liveChatService.sendMessageToManager(message);
+  // }
+  //
+  // sendMessageToCustomer() {
+  //   const message = new Message(this.message, "3", "3");
+  //   this.liveChatService.sendMessageToCustomer(message);
+  // }
 }
--- src/app/services/live-chat-service.service.ts
+++ src/app/services/live-chat-service.service.ts
@@ -1,48 +1,83 @@
 import { Injectable } from '@angular/core';
-import {io} from "socket.io-client";
-import {Observable} from "rxjs";
-import {Message} from "../common/message";
 import {CookieService} from "ngx-cookie-service";
+
+declare var SockJS: any;
+declare var Stomp: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class LiveChatServiceService {
 
-  constructor(private cookieService: CookieService) { }
-  //token to url
-  private jwtToken = "?token=Bearer "+this.cookieService.get('jwtToken');
-
-  private socket = io('http://localhost:8080');
+  public stompClient: any;
+  public msg :String[] = ["dwadawd","dwda"];
+  cookieService: CookieService;
 
 
-  sendMessageToManager(message: Message){
-    this.socket.emit('/ws/send-message-to-shop-manager', message);
+  constructor(cookieService: CookieService) {
+    this.cookieService = cookieService;
+    this.initializeWebSocketConnection();
   }
 
-  sendMessageToCustomer(message: Message){
-    this.socket.emit('/ws/send-message-to-customer', message);
-  }
-
-  getMessagesFromManager() {
-    let observable = new Observable<{content:String}>(observer => {
-      this.socket.on('/topic/messages-from-manager', (data) => {
-        observer.next(data);
+  initializeWebSocketConnection() {
+    const serverUrl = 'http://localhost:8080/socket?token=Bearer '+ this.cookieService.get('jwtToken');
+    const ws = new SockJS(serverUrl);
+    this.stompClient = Stomp.over(ws);
+    const that = this;
+    this.stompClient.connect({}, function (frame: any) {
+      that.stompClient.subscribe('/user/topic/messages-from-manager', (message:any) => {
+        if (message) {
+          console.log("wiadomosc dodawana do listy:")
+          console.log(JSON.parse(message.body).content);
+          that.msg.push(JSON.parse(message.body).content);
+        }
       });
-      return () => { this.socket.disconnect(); };
     });
-    return observable;
   }
 
-  getMessagesFromCustomer() {
-    let observable = new Observable<{content:String}>(observer => {
-      this.socket.on('/topic/messages-from-customers', (data) => {
-        observer.next(data);
-      });
-      return () => { this.socket.disconnect(); };
-    });
-    return observable;
+  sendMessage(message: any) {
+    this.stompClient.send('/ws/send-message-to-customer', {}, JSON.stringify(message));
   }
-
 
 }
+
+
+
+
+  // constructor(private cookieService: CookieService) { }
+  // //token to url
+  // private jwtToken = "?token=Bearer "+this.cookieService.get('jwtToken');
+  //
+  // private socket = io('http://localhost:8080');
+  //
+  //
+  // sendMessageToManager(message: Message){
+  //   this.socket.emit('/ws/send-message-to-shop-manager', message);
+  // }
+  //
+  // sendMessageToCustomer(message: Message){
+  //   this.socket.emit('/ws/send-message-to-customer', message);
+  // }
+  //
+  // getMessagesFromManager() {
+  //   let observable = new Observable<{content:String}>(observer => {
+  //     this.socket.on('/topic/messages-from-manager', (data) => {
+  //       observer.next(data);
+  //     });
+  //     return () => { this.socket.disconnect(); };
+  //   });
+  //   return observable;
+  // }
+  //
+  // getMessagesFromCustomer() {
+  //   let observable = new Observable<{content:String}>(observer => {
+  //     this.socket.on('/topic/messages-from-customers', (data) => {
+  //       observer.next(data);
+  //     });
+  //     return () => { this.socket.disconnect(); };
+  //   });
+  //   return observable;
+  // }
+
+
+
